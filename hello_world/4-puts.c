9a11a3df3510b74ee6dca5a98ee366590403668d
Chloexlsx <chloe850350@gmail.com>
2025-06-16 08:17:12 +0000

    task_4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+#include <string.h>
+/*
+ * main - the entry point prints message using put
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
