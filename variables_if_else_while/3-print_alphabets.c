3a5307c4c40559827f506cc913957b39a721591a
Chloexlsx <chloe850350@gmail.com>
2025-06-17 05:54:02 +0000

    task_3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase and then uppercase a-z
+ *
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	char a, A;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (A = 'A'; A <= 'Z'; A++)
+	{
+		putchar(A);
+	}
+	return (0);
+}
