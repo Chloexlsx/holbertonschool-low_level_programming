08cfa56d0543b11e9abf0f481e57c2f54f6634f1
Chloexlsx <chloe850350@gmail.com>
2025-06-17 13:48:21 +0000

    task_0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints
+ *
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	_putchar();
+	return (0);
+}
+
+void _putchar()
+{
+	printf("_putchar\n");
+}
