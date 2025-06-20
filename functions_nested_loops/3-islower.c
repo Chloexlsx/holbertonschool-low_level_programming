19dd079e62344143ce6fe1710ed311ed5eb0a52b
Chloexlsx <chloe850350@gmail.com>
2025-06-20 03:17:55 +0000

    task_3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * function int  _islower() -
- * checks if the int c passes in the function is lowercaer;
+ * checks if the int @c passes in the function is lowercaer;
  *
  * Return: 1 if yes, otherwise 0 will be returned
  *
