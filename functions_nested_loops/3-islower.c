d894c0d03d5c0775b247366b6e4d32f4b5bccd4b
Chloexlsx <chloe850350@gmail.com>
2025-06-20 03:07:33 +0000

    task_3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * int _islower(int c) - checks if the int passes in the function is lowercaer;
+ *
+ * Return: 1 if yes, otherwise 0 will be returned
+ *
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+	return (1);
+else
+	return (0);
+}
