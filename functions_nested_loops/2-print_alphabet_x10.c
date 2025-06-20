4464ad1e2dd375871aab5c01ea15773cf788b83f
Chloexlsx <chloe850350@gmail.com>
2025-06-20 02:35:55 +0000

    task_2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+			_putchar('\n');
+	}
+}
