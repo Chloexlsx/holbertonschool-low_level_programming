e617a1a0e4d7c408c44b73441439d39fdf5db435
Chloexlsx <chloe850350@gmail.com>
2025-06-20 08:32:10 +0000

    task_9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 table;
+ *
+ * Return: a 9 table
+ *
+ */
+void times_table(void)
+{
+	int i, j, num;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			num = i * j;
+			if (num < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(num + '0');
+			}
+			else if (num == 10)
+			{
+				_putchar('1');
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(num / 10 + '0');
+				_putchar(num % 10 + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('$');
+				_putchar('\n');
+			}
+		}
+	}
+}
