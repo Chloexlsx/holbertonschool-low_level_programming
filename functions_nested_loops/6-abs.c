4ab7eabfc9ae9fea3ff668f9fbecc4a6c1de1b77
Chloexlsx <chloe850350@gmail.com>
2025-06-20 04:40:13 +0000

    task_6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the arg to be an absolute
+ * @i: the formal parameter to check
+ *
+ * Return: an absolute value
+ *
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	return (i);
+}
