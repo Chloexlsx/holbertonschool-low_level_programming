0442e7923ee8f06a9e36a667d6ac4a8a006f9e68
Chloexlsx <chloe850350@gmail.com>
2025-06-20 08:37:57 +0000

    task_10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - prints the addition of two digits ;
+ * @a: the formal parameter to add
+ * @b: the second formal para to add
+ *
+ * Return: the result of the funciton
+ *
+ */
+int add(int a, int b);
+{
+	return (a + b);
+}
