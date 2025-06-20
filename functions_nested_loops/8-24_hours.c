bec3a6b661155d57fd6230000bc6d47603077383
Chloexlsx <chloe850350@gmail.com>
2025-06-20 06:37:52 +0000

    task_8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * jack_bauer - prints prints every minute starting from 00:00 to 23:59.
+ *
+ * Return: 0 always (success)
+ */
+
+void jack_bauer(void)
+{
+int i, j, k, l;
+for (j = 0; j < 3; j++)
+{
+	if (j < 2)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(j + '0');
+					_putchar(i + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	} else
+		for (i = 0; i < 4; i++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(j + '0');
+					_putchar(i + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					if (i == 3 && k == 5 && l == 9)
+						return;
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
