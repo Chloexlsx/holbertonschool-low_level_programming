ca9a28a3112e96cad6fb0e8da3f229e27c275383
Chloexlsx <chloe850350@gmail.com>
2025-06-23 07:41:51 +0000

    task_9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0-100, but the *3 prints fixx and *5 buzz,
+ *			 follow by a newline
+ *
+ *
+ * Return: void
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+			putchar(' ');
+	}
+	putchar('\n');
+}
