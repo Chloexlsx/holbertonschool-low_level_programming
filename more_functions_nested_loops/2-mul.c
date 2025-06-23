a9897a8affe1a89d6c87ddef2bd135c360692e2d
Chloexlsx <chloe850350@gmail.com>
2025-06-23 05:20:58 +0000

    task_2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers
+ * @a: the formal parameter for the function
+ * @b: the other formal para for the function
+ *
+ * Return: the result of the function
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
