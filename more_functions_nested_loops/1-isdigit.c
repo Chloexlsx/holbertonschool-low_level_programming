58e957fdf10113c533e1bcc719fa28d38d285e85
Chloexlsx <chloe850350@gmail.com>
2025-06-23 05:10:06 +0000

    task_1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks for a didgit
+ * @c: the formal parameter for checking
+ *
+ * Return: 1 if true, otherwise 0
+ */
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
