a859b5f65054acb932b4f4e51d9e873cdbc9fdf2
Chloexlsx <chloe850350@gmail.com>
2025-06-24 12:22:32 +0000

    task_0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: the formal parameter to be linked
+ * @src: the number of element to linked
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	while (dest[i] != 0)
+	{
+		i++;
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
