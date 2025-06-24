03b6c255486e85c2fbf6701c69c1d83ea65ecf34
Chloexlsx <chloe850350@gmail.com>
2025-06-24 09:46:16 +0000

    task_5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
+ * rev_string - prints a string in reverse, followed by a new line
  * @s: the formal parameter to be printed
  *
  * Return: void
  */
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int i = 0;
 	int j;
