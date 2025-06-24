24562415d87d1675faff60b2fedac936b091c087
Chloexlsx <chloe850350@gmail.com>
2025-06-24 10:47:19 +0000

    task_9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - copies the string pointed to by src, including (\0) ,to by dest.
+ *		,followed by a new line
+ * @dest: the formal parameter to be printed
+ * @src: the string pointed to
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
