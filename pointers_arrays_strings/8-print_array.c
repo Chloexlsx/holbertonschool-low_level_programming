23c4c8d47d320a40f1553795356b1fa8be1a59c0
Chloexlsx <chloe850350@gmail.com>
2025-06-24 10:20:14 +0000

    task_8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints n element of a string
+ *		,followed by a new line
+ * @a: the formal parameter to be printed
+ * @n: the number of element to print
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; a[i] < n; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+}
