96c29892e6d2477dc1ba8f288bf216c7e1330cfa
Chloexlsx <chloe850350@gmail.com>
2025-06-24 08:44:17 +0000

    task_3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: the formal parameter to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		_putchar(str[len]);
+	}
+	_putchar('\n');
+}
+
