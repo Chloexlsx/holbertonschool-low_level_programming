b6402dfef7f2df00d65ace06d8899178bdd51198
Chloexlsx <chloe850350@gmail.com>
2025-07-01 04:55:55 +0000

    task_0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memset -  fills memory with a constant byte
+ * @b: the constant byte
+ * @n: the first n bytes of the memory area
+ * @s: the pointer points to the string
+ *
+ * Return: a char pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i = i + 1;
+	}
+
+return (s); 
+}
