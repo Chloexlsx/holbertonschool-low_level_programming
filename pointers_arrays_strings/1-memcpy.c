b57677910a6d832e4656a4fe235f8e04d2780822
Chloexlsx <chloe850350@gmail.com>
2025-07-01 05:20:49 +0000

    task_1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memcpy -  copies memory area to another memory area
+ * @dest: the pointer to the destination area
+ * @src: the pointer to the src of the memory area
+ * @n: the number of bytes to copy
+ *
+ * Return: a char pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src [i];
+		i = i + 1;
+	}
+return (dest);
+}
