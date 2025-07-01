2ef06f95161407c494d0ab10b02c108fa48c477d
Chloexlsx <chloe850350@gmail.com>
2025-07-01 12:31:21 +0000

    task_4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: the initial segment
+ * @accept: a string
+ *
+ * Return: an unsigned integer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				return (s + i);
+			}
+			j = j + 1;
+		}
+		i = i + 1;
+	}
+return (NULL);
+}
