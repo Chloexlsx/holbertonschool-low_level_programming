2bd56badd5ba39d56fb8e4800e9dd5bbb54575b0
Chloexlsx <chloe850350@gmail.com>
2025-07-03 11:30:42 +0000

    task_5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * sqrt_helper - recursively tries successive integers to find root of n
+ * @n: the target number
+ * @i: the current number try to find i * i == n
+ *
+ * Return: i if found it;
+ *		-1 if i * i is already pass n, so not found
+ *		otherwise recurse with i + 1
+ */
+static int sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (sqrt_helper(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number
+ *
+ * Return: an integer
+ */
+int _sqrt_recursion(int n);
+{
+	if (n < 0)
+		return (-1);
+	return (sqrt_helper(n, 0));
+}
