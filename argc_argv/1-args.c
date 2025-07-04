3fb7c2f6b4cbb7032c01fbf26a79ec11383e9837
Chloexlsx <chloe850350@gmail.com>
2025-07-04 01:34:39 +0000

    task_1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of itself
+ * @argc: the number of arguments
+ * @argv: the array for the arguments
+ *
+ * Return: 0 if sucess.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
