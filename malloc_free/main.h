176741600d80438f1c73cf8242ebf1602064781e
Chloexlsx <chloe850350@gmail.com>
2025-07-08 04:49:03 +0000

    task_main.h

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -28,4 +28,5 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
