cc63cf9f84fbabf49d076bf41fd91bf3f08f02cb
Chloexlsx <chloe850350@gmail.com>
2025-07-10 01:55:54 +0000

    task_main.h

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -29,4 +29,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
