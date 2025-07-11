47d63689d373ad3d2804ffeb14448f3f105b338d
Chloexlsx <chloe850350@gmail.com>
2025-07-11 03:25:37 +0000

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifdef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner's name
+ *
+ * Description: basic information about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
