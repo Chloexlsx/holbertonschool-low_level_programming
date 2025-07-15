2550a3f6d97a76f29dafe92c5d977dd6632ec758
Chloexlsx <chloe850350@gmail.com>
2025-07-15 01:19:16 +0000

    task_0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointer.h"
+#include <stdio.h>
+
+/**
+ *print_name - prints a name
+ *@name: char pointer
+ *@f: the function pointer
+ *
+ *Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	/*if either name of f is NULL, do nothing*/
+	if (name == NULL || f == NULL)
+		return;
+
+	/*invoke the callback to do the actual printing*/
+	f(name);
+}
