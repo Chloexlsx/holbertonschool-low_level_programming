4cecb2fde1682dac045c8568be77617a5b7707a4
Chloexlsx <chloe850350@gmail.com>
2025-07-30 07:13:21 +0000

    task_2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_node - make a new node
+ * @t: the data to store
+ *
+ * Return: the pointer to the new node
+ */
+dlistint_t *new_node(int t)
+{
+	dlistint_t *ptr;
+
+	ptr = malloc(sizeof(dlistint_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->n = t;
+	ptr->prev = NULL;
+	ptr->next = NULL;
+	return (ptr);
+}
+
+/**
+ * dlistint_t *add_dnodeint- add a new node in the beginning of a dlinklist
+ * @headptr: the iterator pointer points to a dlistint_t
+ * @n: an integer
+ * Return: address of the new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **headptr, const int n)
+{
+	dlistint_t *new_node_ptr;
+	dlistint_t *head = *headptr;
+
+	new_node_ptr = new_node(n);
+	if (new_node_ptr == NULL)
+		return (NULL);
+	head->prev = new_node_ptr;
+	new_node_ptr->next = head;
+
+	/**
+	* for line *headptr = new_node_ptr; I used head = new_node_ptr; initially,
+	*	however, it does not work, it will not change the actual head
+	*	value in main(). Only thru headptr, because when the function
+	*	is called, the actual head in the main can only be accessed
+	*	using headptr. Changing the value of head I declare
+	*	in add_dnoteint will not change the head in main.
+	*/
+	*headptr = new_node_ptr;
+	return (new_node_ptr);
+}
