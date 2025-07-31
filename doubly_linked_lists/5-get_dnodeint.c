4a7ca09ac42ef45caf907ef1cd659c466ba1a0d4
Chloexlsx <chloe850350@gmail.com>
2025-07-31 01:35:25 +0000

    task_5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (i < index)
 	{
 		if (temp == NULL)
-			return (NULL)
+			return (NULL);
 		temp = temp->next;
 		i++;
 	}
