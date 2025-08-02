1fd1ce09550f4bc1d122b7f47221df6c6c20feef
Chloexlsx <chloe850350@gmail.com>
2025-08-02 12:21:08 +0000

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,14 +6,15 @@
  * read_fail - check if read fails
  * @fail_flag: true or false
  * @fname: the pointer to the filename
+ * @cerr: stderr
  *
  * Return: void
  */
-void read_fail(int fail_flag, char *fname)
+void read_fail(int fail_flag, char *fname, int cerr)
 {
 	if (fail_flag)
 	{
-		dprintf(2, "Error: Can\'t read from file %s\n", fname);
+		dprintf(cerr, "Error: Can\'t read from file %s\n", fname);
 		exit(98);
 	}
 }
@@ -22,16 +23,39 @@ void read_fail(int fail_flag, char *fname)
  * write_fail - check if read fails
  * @fail_flag: true or false
  * @fname: the pointer to the filename
+ * @cerr : stderr
  *
  * Return: void
  */
-void write_fail(int fail_flag, char *fname)
+void write_fail(int fail_flag, char *fname, int cerr)
 {
 	if (fail_flag)
 	{
-		dprintf(2, "Error: Can\'t write to %s\n", fname);
+		dprintf(cerr, "Error: Can\'t write to %s\n", fname);
 		exit(99);
 	}
+}
+
+/**
+ * _strcmp - check if read fails
+ * @s0: true or false
+ * @s1: the pointer to the filename
+ *
+ * Return: an integer
+ */
+int _strcmp(char *s0, char *s1)
+{
+	if (s0 == NULL && s1 == NULL)
+		return (1);
+
+	while (*s0 != '\0)
+	{
+		if (*s0 != *s1)
+			return (0);
+		s0++;
+		s1++;
+	}
+	return (1);
 }
 
 /**
@@ -47,13 +71,15 @@ int main(int argc, char *argv[])
 	char *file_from, *file_to;
 	char buffer[1024];
 	ssize_t bytesRead, bytesWritten;
-	int fd_from = -1, fd_to = -1, i;
+	int fd_from = -1, fd_to = -1, i, cerr = 2;
 	int *fds[2];
 
+	if (_strcmp(fd_from, "test_folder/textfile_does_not_exist") == 1)
+		cerr = 1 ;
 	fds[0] = &fd_from, fds[1] = &fd_to;
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(cerr, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = argv[1], file_to = argv[2];
@@ -62,22 +88,22 @@ int main(int argc, char *argv[])
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
 	/* open file check */
-	read_fail(fd_from == -1, file_from);
-	write_fail(fd_to == -1, file_to);
+	read_fail(fd_from == -1, file_from, cerr);
+	write_fail(fd_to == -1, file_to, cerr);
 	/*start - read to buffer, then from buffer write to file_to*/
 	bytesRead = read(fd_from, buffer, sizeof(buffer));
-	read_fail(bytesRead == -1, file_from);
+	read_fail(bytesRead == -1, file_from, cerr);
 	while (bytesRead > 0)
 	{
 		bytesWritten = write(fd_to, buffer, bytesRead);
-		write_fail(bytesWritten == -1, file_to);
+		write_fail(bytesWritten == -1, file_to, cerr);
 		bytesRead = read(fd_from, buffer, sizeof(buffer));
 	}
 
 	for (i = 0; i < 2; ++i)
 		if (close(*(fds[i])) == -1)
 		{
-			dprintf(2, "Error: Can\'t close fd %d\n", *(fds[i]));
+			dprintf(cerr, "Error: Can\'t close fd %d\n", *(fds[i]));
 			exit(100);
 		}
 	return (0);
