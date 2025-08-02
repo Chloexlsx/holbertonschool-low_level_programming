73fc33ca8edb9ef0011d29051b5685dd14233dc9
Chloexlsx <chloe850350@gmail.com>
2025-08-02 00:49:40 +0000

    task_0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/types.h>
+
+/**
+ * read_textfile - read text file then write the content to POXIX
+ * @filename: the filename
+ * @letters: the bytes to read and write
+ *
+ * Return: the actual number of letters it could read and print
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	/*open() will return a file descriptor for RW function to look for the file*/
+	int fd = open(filename, O_RDONLY);
+	/*ask for the bytes according to the input argument from malloc*/
+	char *buffer = malloc(letters);
+	ssize_t bytesRead, bytesWritten;
+
+	if (buffer == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+	if (fd == -1)
+		return (0);
+	/*read bytes given from the file given into buffer, using file descriptor*/
+	/*read() returns how many bytes read to the variable bytesRead*/
+	bytesRead = read(fd, buffer, letters);
+
+	if (bytesRead == -1)
+		return (0);
+	close(fd);
+	/*prints char in buffer based on how many bytes are read successfully*/
+	bytesWritten = write(1, buffer, bytesRead);
+
+	if (bytesWritten == -1)
+		return (0);
+	return (bytesWritten);
+}
